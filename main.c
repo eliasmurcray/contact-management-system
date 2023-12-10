7ed9868cc8dde34487e5cfdde230b84f5b003911
Elias Murcray <eliasmurcray@gmail.com>
2023-12-10 15:48:37 -0800

    Structs

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+struct DiscordUser {
+	char name[40];
+	long id;
+};
 
 int main() {
-	printf("Works\n");
+	struct DiscordUser* users = (struct DiscordUser*)malloc(2 * sizeof(struct DiscordUser));
+
+	strcpy(users[0].name, "Aliquis");
+	users[0].id = 163437468522250240;	
+
+	strcpy(users[1].name, "Vexcess");
+	users[1].id = 480905025112244234;
+			
+	for (int i = 0; i < 2; i++) {
+		printf("%s (%ld)\n", users[i].name, users[i].id);
+	}
+		
 	return 0;
 }
