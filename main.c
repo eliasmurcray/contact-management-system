734acc36a2fe37fc3051344da5eb91abad96a034
ejmurc <eliasjmurcray@Eliass-MacBook-Pro.local>
2023-12-10 21:19:55 -0800

    Dynamic memory allocation

--- main.c
+++ main.c
@@ -7,18 +7,28 @@ struct DiscordUser {
 	long id;
 };
 
+void add_user(struct DiscordUser** users, size_t* len, char* name, long id) {
+	struct DiscordUser new_user;
+	strcpy(new_user.name, name);
+	new_user.id = id;
+	*users = (struct DiscordUser*) realloc(*users, 1 + *len);
+	(*users)[*len] = new_user;
+	*len += 1;
+}
+
 int main() {
 	struct DiscordUser* users = (struct DiscordUser*)malloc(2 * sizeof(struct DiscordUser));
-
-	strcpy(users[0].name, "Aliquis");
-	users[0].id = 163437468522250240;	
-
-	strcpy(users[1].name, "Vexcess");
-	users[1].id = 480905025112244234;
-			
-	for (int i = 0; i < 2; i++) {
+	
+	size_t len = 0;
+	add_user(&users, &len, "Aliquis", 163437468522250240);
+	add_user(&users, &len, "Vexcess", 480905025112244234);
+	add_user(&users, &len, "WKoA", 724416180097384498);
+	
+	for (int i = 0; i < len; i++) {
 		printf("%s (%ld)\n", users[i].name, users[i].id);
 	}
-		
+	
+	free(users);
+	
 	return 0;
 }
